36600149c5f63d5f436e68ae476a647934be00d1
Luna <0510winnie@gmail.com>
2019-01-13 20:46:54 +0800

    create question form and display session messages

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,14 +5,25 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">All Questions</div>
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h2>
+                            All Questions
+                        </h2>
+                        <div class="ml-auto">
+                            {{-- ml-auto so the button will be positioned at the right --}}
+                            <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask questions</a>
+                        </div>
+                    </div>
+                </div>
 
                 <div class="card-body">
+                    @include('layouts._messages')
                     @foreach ($questions as $question)
                     <div class="media">
                         <div class="d-flex flex-column counters">
                             <div class="vote">
-                                <strong>{{ $question->votes }}</strong> {{ str_plural('vote', $question->votes)}}
+                                <strong>{{ $question->votes }}</strong> {{ str_plural('vote', $question->votes) }}
                             </div>
                             <div class="status {{ $question->status }}">
                                 <strong>{{ $question->answers }}</strong> {{ str_plural('answer', $question->answers)}}
@@ -23,8 +34,12 @@
                         </div>
                         <div class="media-body">
                             <h3 class="mt-0">
-                                {{ $question->title }}
+                                <a href="{{ $question->url }}">{{ $question->title }}</a>
                             </h3>
+                            <p class="lead">
+                                Asked by    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                <small class="text-muted">{{ $question->created_date }}</small>
+                            </p>
                             {{ str_limit($question->body, 250) }}
                         </div>
                     </div>
