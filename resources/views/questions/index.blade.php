b310f3cf05efe36fa26c540529904be3fe6dabfa
Luna <0510winnie@gmail.com>
2019-01-20 11:16:09 +0800

    tidying up the views

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -19,64 +19,13 @@
                     @include ('layouts._messages')
 
                     @forelse ($questions as $question)
-                        <div class="media">
-                            <div class="d-flex flex-column counters">
-                                <div class="vote">
-                                    <strong>{{ $question->votes_count }}</strong> {{ str_plural('vote', $question->votes_count ) }}
-                                </div>
-                                <div class="status {{ $question->status }}">
-                                    <strong>{{ $question->answers_count }}</strong> {{ str_plural('answer', $question->answers_count) }}
-                                </div>
-                                <div class="view">
-                                    {{ $question->views . " " . str_plural('view', $question->views) }}
-                                </div>
-                            </div>
-                            <div class="media-body">
-                                <div class="d-flex align-items-center">
-                                    <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
-                                    <div class="ml-auto">
-                                        {{-- @if(Auth::user()->can('update-question', $question)) --}}
-                                        {{-- @if(Auth::user()->can('update', $question)) --}}
-                                        @can('update', $question)
-                                        <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                        @endcan
-                                        {{-- @endif --}}
-                                        {{-- @endif --}}
-
-
-                                        {{-- @if(Auth::user()->can('delete-question', $question)) --}}
-                                        {{-- @if(Auth::user()->can('delete')) --}}
-                                        @can('delete')
-                                        <form action="{{ route('questions.destroy', $question->id) }}" method="post" class="form-delete">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                                        </form>
-                                        @endcan
-                                        {{-- @endif --}}
-                                        {{-- @endif --}}
-                                    </div>
-                                </div>
-                                <p class="lead">
-                                    Asked by
-                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                    <small class="text-muted">{{ $question->created_date }}</small>
-                                </p>
-                                <div class="excerpt">
-                                    {{ $question->excerpt(300) }}
-                                </div>
-                            </div>
-                        </div>
-                        <hr>
+                        @include ('questions._excerpt')
                     @empty
                         <div class="alert alert-warning">
                             <strong>Sorry, there are no answers available.</strong>
                         </div>
                     @endforelse
-
-                    <div class="mx-auto">
-                        {{ $questions->links() }}
-                    </div>
+                     {{ $questions->links() }}
                 </div>
             </div>
         </div>
